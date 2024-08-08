d835dc363c20dc782b507a652a152d4dec30b7f1
Wencan Peng <p.wencan@outlook.com>
2024-08-08 09:20:29 +0000

    Initial commit: upload initial configuration and dependencies

---
+++ src/main/java/me/wencanp/onlineLib/OnlineLibApplication.java
@@ -0,0 +1,14 @@
+package me.wencanp.onlineLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(OnlineLibApplication.class, args);
+		System.out.println("Hello Remote!");
+	}
+
+}
---
+++ src/test/java/me/wencanp/onlineLib/OnlineLibApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wencanp.onlineLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
